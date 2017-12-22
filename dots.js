173de28995fba8bd018cc49b172ec4c869b5a38f
InkFx <davidsantana409@yahoo.com>
2017-12-21 18:46:21 -0800

    setting up color show

--- dots.js
+++ dots.js
@@ -1,47 +1,10 @@
 
 
 class Dot {
-  constructor(_id, _name, _hex, _x, _y) {
-    this.id = _id;
-    this.name = _name;
-    this.hex = _hex;
-    this.x = _x;
-    this.y = _y;
-    this.xSpeed = random(-3,3);
-    this.ySpeed = random(-5, 5);
-    this.nearestDots = [];
+  constructor(_color) {
+    this.color = _color;
   }
 
-  paint() {
-    stroke(255);
-    fill(this.hex);
-    ellipse(this.x, this.y, 25);
-
-    noStroke();
-    fill(255);
-    text(this.name, this.x - 20, this.y - 20);
-  }
-
-  move() {
 
 
-    if (this.x > width || this.x < 0) {
-      this.xSpeed = -this.xSpeed;
-    }
-
-    if (this.y > height || this.y < 0) {
-      this.ySpeed = -this.ySpeed;
-    }
-
-    this.x += this.xSpeed;
-    this.y += this.ySpeed;
-
-  }
-
-  measureDistance(_other) {
-    let other = _other;
-    let distance = dist(this.x, this.y, other.x, other.y);
-
-    return distance;
-  }
 }
--- sketch.js
+++ sketch.js
@@ -1,86 +1,17 @@
-let jsonPage = 'https://raw.githubusercontent.com/dariusk/corpora/master/data/colors/crayola.json';
-let colorData;
-let dots = [];
-let dot;
-let idList = {};
-let reduction = 100;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  loadJSON(jsonPage,setData);
+
 }
 
-function setData(data) {
-  colorData = data;
-}
+
 
 
 
 function draw() {
-  background(0);
-  pushDots();
-  for(let i = 0; i < dots.length ; i++) {
-    dots[i].paint();
-    dots[i].move();
-  }
-  listNames();
-  findClosest();
-  //checkClosest();
-
 
 }
 
 
-function pushDots() {
-  if (colorData) {
-    if (dots.length != colorData.colors.length - reduction) {
-      for (let i = 0; i < colorData.colors.length - reduction; i++) {
-        let col = colorData.colors[i].color;
-        let hex = colorData.colors[i].hex;
-        dot = new Dot(i, col, hex, random(width), random(height));
-        dots.push(dot);
-      }
-    }
-  }
-  dot = null;
-}
-
-
-function listNames() {
-  if  (idList[0] == null) {
-    for (let i = 0; i < dots.length; i++ ) {
-      let id = dots[i].id;
-      idList[id] = dots[i].name;
-
-    }
-  }
-}
-
-function findClosest() {
-
-  for (let i = 0; i < dots.length; i++) {
-    for (let j = 0; j < dots.length; j++) {
-      if (i != j) {
-        if (dots[i].measureDistance(dots[j]) < 65 && !dots[i].nearestDots.includes(dots[j])) {
-          stroke(255,0,0);
-          line(dots[i].x, dots[i].y, dots[j].x, dots[j].y);
-          dots[i].nearestDots.push(dots[j]);
-          }
-        }
-        dots[9].nearestDots.forEach(checkClosest);
-      }
-    }
-  }
-
-
-  function checkClosest(nearestDot) {
-    let indexOfNearest;
-    let distance = dist(dots[9].x, dots[9].y, nearestDot.x, nearestDot.y);
-    //for (let i = dots.length; i = 0; i--) {
-    if (distance > 70) {
-    //  indexOfNearest = dots[9].nearestDots.indexOf(nearestDot);
-
-    }
-   //print(distance);
-  //  }
-  }
+f

3a76bf97788837505ab5a93f59556a50b8c20ecf
InkFx <davidsantana409@yahoo.com>
2017-12-22 16:24:49 -0800

    Lights

---
+++ red.js
@@ -0,0 +1,30 @@
+
+class RedBeam {
+  constructor(_x, _y, _rad) {
+    this.x = _x;
+    this.y = _y;
+    this.rad = _rad;
+    this.xSpeed = 3;
+    this.ySpeed = 3;
+  }
+
+  paint() {
+    fill(255, 0, 0, 90);
+    ellipse(this.x, this.y, this.rad);
+  }
+
+  update() {
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+    this.checkPos();
+  }
+  checkPos() {
+    if (this.x - this.rad/2 < 0 || this.x + this.rad/2 > width ||  this.x < 0 || this.x > width ) {
+      this.xSpeed *= -1;
+    }
+
+    if (this.y < 0 || this.y > height || this.y - this.rad/2 < 0 || this.y + this.rad / 2 > height) {
+      this.ySpeed *= -1;
+    }
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,11 +1,46 @@
-
+let redBeams = [];
+let greenBeams = []
+let blueBeams = [];
+let redBeam, greenBeam, blueBeam;
 
 
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+  redBeam = new RedBeam(random(width - 200), random(height - 200), 400);
+  redBeams.push(redBeam);
 
+  greenBeam = new GreenBeam(random(width - 200), random(height - 200), 400);
+  greenBeams.push(greenBeam);
 
+  blueBeam = new BlueBeam(random(width - 200), random(height - 200), 400);
+  blueBeams.push(blueBeam);
 }
 
 function draw() {
+  background(0);
+  goGreen();
+  goRed();
+  goBlue();
+}
 
+
+function goGreen() {
+  for (greenB of greenBeams) {
+    greenB.paint();
+    greenB.update();
+  }
+}
+
+function goBlue() {
+  for (blueB of blueBeams) {
+    blueB.paint();
+    blueB.update();
+  }
+}
+
+function goRed() {
+  for (redB of redBeams) {
+    redB.paint();
+    redB.update();
+  }
 }
